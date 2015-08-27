bb94c6f51b668dfeaffb800d9148c3a5670112fb
Alfonso Kim <alfonsokim84@gmail.com>
2015-08-27 16:07:19 -0500

    avances

--- import.py
+++ import.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import pickle
 from collections import namedtuple
 
 ## ============================================================================
@@ -7,12 +8,11 @@ Column = namedtuple('Column', 'name index size')
 STB = Column('STB', 0, 64)
 TITLE = Column('TITLE', 1, 64)
 PROVIDER = Column('PROVIDER', 2, 64)
-DATE = Column('DATE', 3, 64)
-REV = Column('REV', 4, 64)
-VIEW_TIME = Column('VIEW_TIME', 5, 64)
-LINE_ITEMS = 6
+DATE = Column('DATE', 3, 10)
+REV = Column('REV', 4, 10)
+VIEW_TIME = Column('VIEW_TIME', 5, 10)
+COLUMNS = [STB, TITLE, PROVIDER, DATE, REV, VIEW_TIME]
 INDEX_COLUMNS = [STB.index, TITLE.index]
-COLUMN_NAMES = 'STB|TITLE|PROVIDER|DATE|REV|VIEW_TIME'.split('|')
 
 ## ============================================================================
 def _debug(line, options):
@@ -27,16 +27,26 @@ def _parse_line(c, line, datastore, options):
     """
     """
     fields = line.strip().split('|')
-    if len(fields) != LINE_ITEMS:
+    if len(fields) != len(COLUMNS):
         raise ValueError('%s' % line)
     _debug(fields, options)
     indexes = datastore['indexes']
     for f, field in enumerate(fields):
         if f in INDEX_COLUMNS:
-            _debug('indexando %s: %s' % (COLUMN_NAMES[f], field), options)
-            key_index = indexes[COLUMN_NAMES[f]].get(field, [])
+            column = COLUMNS[f]
+            _debug('indexando %s: %s' % (column.name, field), options)
+            key_index = indexes[column.name].get(field, [])
             key_index.append(c)
-            indexes[COLUMN_NAMES[f]][field] = key_index
+            indexes[column.name][field] = key_index
+
+## ============================================================================
+def _save(datastore, options):
+    """
+    """
+    _debug('saving datastore %s' % str(datastore), options)
+    ds_file = open('datastore.ds', 'wb')
+    pickle.dump(datastore, ds_file)
+    ds_file.close()
 
 ## ============================================================================
 def import_stream(stream, options):
@@ -44,14 +54,12 @@ def import_stream(stream, options):
     """
     data_file = open('data', 'wb')
     index_file = open('index', 'w')
-    datastore = {'d': data_file, 'i': index_file, 
-                 'indexes': {COLUMN_NAMES[f]: {} for f in INDEX_COLUMNS}}
-    print datastore
+    datastore = {'indexes': {COLUMNS[f].name: {} for f in INDEX_COLUMNS}}
     for c, line in enumerate(stream):
         _debug('%i: %s' % (c, line.strip()), options)
         _parse_line(c, line, datastore, options)
-    # -------------------------------------------------------------------------
-    _debug('datastore: %s' % str(datastore), options)    
+    # -------------------------------------------------------------------------  
+    _save(datastore, options)
     data_file.close()
     index_file.close()
 
